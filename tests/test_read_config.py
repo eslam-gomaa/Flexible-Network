3c722c3f9a852b210fffcaf019547731cd228bc8
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-02-10 22:49:21 +0000

    Add unit tests

--- tests/test_read_config.py
+++ tests/test_read_config.py
@@ -3,6 +3,7 @@ from Flexible_Network.read_config import Config
 
 config = Config()
 
+
 def test_section_vault():
     """ Test case to validate that 'section_vault' returns dict """
     assert isinstance(config.section_vault(), dict)
@@ -17,3 +18,7 @@ def test_section_s3():
     """ Test case to validate that 'section_s3' returns dict """
     assert isinstance(config.section_s3(), dict)
 
+def test_not_existed_section():
+    """ Test case to validate that not existed sections raise ValueError """
+    with pytest.raises(ValueError):
+        config.section_vault(section_name="does_not_exist")
