3420d3abcdae7847927e92ee72726b38c2bcb43a
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-02-10 22:05:55 +0000

    Update unit tests for Config class

--- tests/test_read_config.py
+++ tests/test_read_config.py
@@ -8,5 +8,12 @@ def test_section_vault():
     assert isinstance(config.section_vault(), dict)
 
 
+def test_section_rocket_chat():
+    """ Test case to validate that 'section_rocket_chat' returns dict """
+    assert isinstance(config.section_rocket_chat(), dict)
 
+
+def test_section_s3():
+    """ Test case to validate that 'section_s3' returns dict """
+    assert isinstance(config.section_s3(), dict)
 
