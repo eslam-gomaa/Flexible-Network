813aae9435bb20dcf8cc9d8e6d3b06c42736306b
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-02-13 11:53:11 +0000

    Before modifications

--- Flexible_Network/ssh_authentication.py
+++ Flexible_Network/ssh_authentication.py
@@ -10,12 +10,16 @@ class SSH_Authentication():
         self.info = {}
         self.info['is_connected'] = False
         self.info['tries'] = 0
-        # self.info['channel'] = None
         self.info['Fail_Reason'] = ""
         self.info['time_to_connect_seconds'] = None
 
 
     def connect(self, host, user, password, port=22, ssh_timeout=15, allow_agent=True, max_tries=5):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.port = port
+
         self.info['host'] = host
         self.info['max_tries'] = max_tries
         self.info['ssh_timeout'] = ssh_timeout
@@ -29,11 +33,11 @@ class SSH_Authentication():
                 self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 self.ssh.connect(host, port, user, password, timeout=ssh_timeout,
                                     allow_agent=allow_agent, look_for_keys=False)
-                channel = self.ssh.invoke_shell()
-                self.info['is_connected'] = True
+                self.channel = self.ssh.invoke_shell()
+                self.is_connected = True
 
-                output = channel.recv(9999)
-                channel.send_ready()
+                output = self.channel.recv(9999)
+                self.channel.send_ready()
                 time.sleep(1)
                 break
             except paramiko.AuthenticationException as e:
@@ -56,10 +60,19 @@ class SSH_Authentication():
         time_end = datetime.datetime.now()
         time_taken = time_end - time_start
         self.info['time_to_connect_seconds'] = time_taken.seconds
-        data = {}
-        data['ssh_session'] = self.ssh
-        data['data'] = self.info
-        return data
+        self.info['is_connected'] = True
+        return self.info
+
+    def reconnect(self):
+        # reconnect = self.connect(self.host, self.user, self.password, port=22, ssh_timeout=15, allow_agent=True, max_tries=5)
+        # return reconnect
+
+        re1 = self.connect(host=self.host, user=self.user, password=self.password)
+        # self.channel = re1.channel
+        # self.ssh = re1.ssh
+        return re1
+
+
 
     def close(self):
         """
