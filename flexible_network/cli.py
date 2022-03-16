9775efb06507711b7367d52f50a2d71eecc03dfa
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-03-16 15:56:33 +0000

    comment --validate-integrations -> To fix its bug later

--- flexible_network/cli.py
+++ flexible_network/cli.py
@@ -1,4 +1,5 @@
 import argparse
+from asyncio import tasks
 from Flexible_Network import ReadCliOptions
 # from flexible_network.read_cli_options import ReadCliOptions
 
@@ -11,25 +12,37 @@ class CLI:
         parser = argparse.ArgumentParser(description='A Python tool that to automate network devices with much flexibility & lots of integrations')
         parser.add_argument('-n', '--name', type=str, required=True, metavar='', help='The Task Name')
         parser.add_argument('-i', '--inventory', type=str, required=False, metavar='', help='The inventory file')
-        parser.add_argument('-V', '--validate-integration', nargs='+',choices=['cyberArk', 'rocketChat'] , required=False, metavar='', help='Test API Integrations')
+        # parser.add_argument('-V', '--validate-integration', nargs='+',choices=['cyberArk', 'rocketChat'], help='Test API Integrations')
         parser.add_argument('-x', '--no-confirm-auth', action='store_true', help='Skip Asking for confirmation if failed to connect to some deivces')
         parser.add_argument('-c', '--config', type=str, required=False, metavar='', help='The path of configuration file')
         parser.add_argument('-g', '--authenticate-group', type=str, required=False, metavar='', help='Authenticate an inventory group')
         parser.add_argument('-u', '--user', type=str, required=False, metavar='', help='The user to authenticate the group')
         parser.add_argument('-p', '--password', type=str, required=False, metavar='', help='The password to authenticate the group')
         parser.add_argument('-P', '--port', type=int, required=False, default=22, metavar='', help='The port to connect to the group')
+        parser.add_argument('-l', '--tasks', action='store_true', help='List tasks')
+        parser.add_argument('-b', '--backups', action='store_true', help='List backups')
 
 
         results = parser.parse_args()
+
+        if results.tasks:
+            ReadCliOptions.list_tasks = True
+            
+        # elif results.list_backups is not None:
+        #     ReadCliOptions.list_backups == True
+
+        # if results.filter_by_date is not None:
+        #     ReadCliOptions.filter_by_date = results.filter_by_date
+
         if results.inventory is not None:
             ReadCliOptions.inventory_file = results.inventory
 
         if results.name is not None:
             ReadCliOptions.task_name = results.name
         
-        if results.validate_integration is not None:
-            # Convert the list to a set to remove the duplicates
-            ReadCliOptions.to_validate_lst = set(results.validate_integration)
+        # if results.validate_integration is not None:
+        #     # Convert the list to a set to remove the duplicates
+        #     ReadCliOptions.to_validate_lst = set(results.validate_integration)
 
         if results.no_confirm_auth:
             ReadCliOptions.no_confirm_auth = True
