3a0c54c81ce22a3dc01e0a6a7c61042d9ebe23b7
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-03-16 06:33:56 +0000

    detect 'Ambiguous command' as an error

--- flexible_network/vendors/cisco.py
+++ flexible_network/vendors/cisco.py
@@ -1,6 +1,8 @@
 
 class Cisco:
     def __init__(self):
+        # If needed check that error messgaes list
+        # https://www.cisco.com/c/en/us/td/docs/security/ips/7-2/command/reference/cmdref72/crError.pdf
         self._stderr_search_keyword = ['\^', '% Ambiguous command']
         self._clean_output_search_keywords = ['.*#', '.*>']
         self._backup_command = """
