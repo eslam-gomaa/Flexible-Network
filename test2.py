36e518bbcc4efa3975d3579592643666bdf49f19
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-03-24 15:02:51 +0000

    Testing

--- test2.py
+++ test2.py
@@ -1,13 +1,13 @@
 from FlexibleNetwork.Flexible_Network import Terminal_Task
 from FlexibleNetwork.Integrations import RocketChat_API
-from FlexibleNetwork.Integrations import Cyberark_APIs_v2
+# from FlexibleNetwork.Integrations import Cyberark_APIs_v2
 
 
 task = Terminal_Task()
 rocket = RocketChat_API()
-cyberark = Cyberark_APIs_v2()
+# cyberark = Cyberark_APIs_v2()
 
-print(cyberark.search_accounts(search='egomaa'))
+# print(cyberark.search_accounts(search='egomaa'))
 
 ##  1  ## Authenticate
 # task.authenticate(hosts=task.inventory_groups['all'], user='orange', password='cisco', port='1113')
@@ -20,7 +20,7 @@ rocket.send_message(['eslam.gomaa'], "``` {} ```".format(report))
 
 ### Test exeuting a command.
 
-cmd ='''sh vlan br
+cmd ='''sh vlan br716
 sh ip int br'''
 
 
@@ -29,5 +29,7 @@ for host in task.connected_devices_dct:
     
     task.execute_raw(host_dct, 'enable\n' + 'cisco')
     task.execute(host_dct, cmd, terminal_print='default')
-    task.backup_config(host_dct, 'Test config backup 1', target='local')    
+    task.execute(host_dct, cmd, terminal_print='json')
+    task.backup_config(host_dct, 'Testing with Dakhli', target='local')    
+    task.backup_config(host_dct, 'before important update', target='local')    
 
