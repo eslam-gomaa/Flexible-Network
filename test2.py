b428bbeab3fe8a02f20e70736c0c733c08cf1d26
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-03-26 19:28:41 +0000

    testing s3 backup

--- test2.py
+++ test2.py
@@ -15,7 +15,7 @@ rocket = RocketChat_API()
 ##  2  ## Get Connection Report
 report = task.connection_report_Table(task.devices_dct)
 # print(report)
-rocket.send_message(['eslam.gomaa'], "``` {} ```".format(report))
+rocket.send_message(['eslam.gomaa', 'Nabawy'], "``` {} ```".format(report))
 
 
 ### Test exeuting a command.
